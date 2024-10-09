d63d404835c220a02fcd4cfa0ccc2bc583e865f2
Juan Volkmar <juan.volkmar@upb.edu.co>
2024-10-09 17:13:46 -0500

    calculadoras

---
+++ app/src/main/java/com/example/beca18calculator/CalculatorsActivity.kt
@@ -0,0 +1,24 @@
+package com.example.beca18calculator
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.beca18calculator.databinding.ActivityCalculatorsBinding
+
+class CalculatorsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCalculatorsBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCalculatorsBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.goCals.setOnClickListener{
+            startActivity(Intent(this@CalculatorsActivity, CalculatorsActivity::class.java))
+        }
+
+        binding.goHome.setOnClickListener{
+            startActivity(Intent(this@CalculatorsActivity, HomeActivity::class.java))
+        }
+    }
+}
--- app/src/main/java/com/example/beca18calculator/HomeActivity.kt
+++ app/src/main/java/com/example/beca18calculator/HomeActivity.kt
@@ -1,14 +1,23 @@
 package com.example.beca18calculator
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.beca18calculator.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding=ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.goCals.setOnClickListener{
+            startActivity(Intent(this@HomeActivity, CalculatorsActivity::class.java))
+        }
+
+        binding.goHome.setOnClickListener{
+            startActivity(Intent(this@HomeActivity, HomeActivity::class.java))
+        }
     }
 }
